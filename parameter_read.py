d38cb21f65e94286fc7824b612553d6dec4cbbd6
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-06-23 21:50:48 +0200

    Create parameter_read.py

---
+++ parameter_read.py
@@ -0,0 +1,7 @@
+from propar import instrument
+
+dev = instrument('/dev/ttyUSB1', address=3, baudrate=38400)
+
+# Get all known DDE parameters
+value_25 = dev.readParameter(25)
+print(f"Parameter 25 value: {value_25}")
