64453eba5a7fd7d94d897d4adfb9618663b289a2
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-07-06 15:45:24 +0200

    Update Fluidindexes.py

--- Fluidindexes.py
+++ Fluidindexes.py
@@ -5,7 +5,7 @@ dev = instrument('/dev/ttyUSB0', address=3, baudrate=38400)
 with open("fluidsets_log.txt", "w", encoding="utf-8") as log:
     log.write("Index | Name         | Density      | ...\n")
     log.write("-" * 50 + "\n")
-    for idx in range(10):
+    for idx in range(15):
         try:
             dev.writeParameter(24, idx)  # Select fluidset index
             name = dev.readParameter(25)  # Fluid name
