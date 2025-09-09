d26a5759b7c0b8b33a1ca4505aa4a5e4d8433409
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-09-09 15:12:48 +0200

    Update models.py

--- propar_qt/models.py
+++ propar_qt/models.py
@@ -85,7 +85,7 @@ class NodesTableModel(QAbstractTableModel):
         if col == 3:
             return node.serial
         if col == 4:
-            return getattr(node, "number")
+            return getattr(node, "number", None)
         return None
 
 
