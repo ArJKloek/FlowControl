52a6de40164e66c159a2725024fe5b1448687f48
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-09-09 14:23:06 +0200

    Update parameter_read.py

--- parameter_read.py
+++ parameter_read.py
@@ -5,6 +5,6 @@ dev = instrument('/dev/ttyUSB1', address=3, baudrate=38400)
 # Get all known DDE parameters
 value_24 = dev.readParameter(24)
 value_25 = dev.readParameter(25)
-value_60 = dev.readParameter(93)
+value_60 = dev.readParameter(115)
 
-print(f"Parameter 24 Value: {value_24} Parameter 25 value: {value_25} Parameter 93 value:{value_60}")
+print(f"Parameter 24 Value: {value_24} Parameter 25 value: {value_25} Parameter 115 value:{value_60}")
