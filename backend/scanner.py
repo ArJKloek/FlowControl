005fe8e9e16db438c4919a0b86e91f624fad810c
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-09-12 12:29:02 +0200

    Update scanner.py

--- backend/scanner.py
+++ backend/scanner.py
@@ -122,6 +122,7 @@ class ProparScanner(QThread):
                         id_str=str(n['id']),
                         channels=int(n['channels'])
                     )
+                    print(f"Found: {info}")  # <-- debug print
                     # Assign a number to each instrument
                     numbered_info = {
                         "number": instrument_counter,
