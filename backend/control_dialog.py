612b4b379be6baa62cc158e436995e1bafe014af
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-09-19 12:40:38 +0200

    Update control_dialog.py

--- backend/control_dialog.py
+++ backend/control_dialog.py
@@ -192,6 +192,7 @@ class ControllerDialog(QDialog):
         try:
             if self._pending_pct is None:
                 return
+            print("Sending pct:", self._pending_pct)
             self.manager.request_setpoint_pct(
                 self._node.port,
                 self._node.address,
