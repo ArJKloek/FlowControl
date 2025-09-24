38a52c6da3ce4af6191322262b0b924b47cc0e63
ArJKloek <123403478+ArJKloek@users.noreply.github.com>
2025-09-24 14:59:54 +0200

    Update worker.py

--- backend/worker.py
+++ backend/worker.py
@@ -65,7 +65,7 @@ class TelemetryLogWorker(QObject):
                     self._fmeasure_buffer.append(val)
         except queue.Empty:
             pass
-
+        print(f'Interval: {self._interval}, average: {self._last_avg_time}, now: {now}')
         if now - self._last_avg_time >= self._interval and self._fmeasure_buffer:
             self._write_average(now)
 
